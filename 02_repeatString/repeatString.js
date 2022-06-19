ac00732ec5ec6910d437bdabab01b0d4e90b5570
Leonardo Loaiza <leonardodario.loaiza@unmsm.edu.pe>
2022-06-19 14:32:35 -0500

    Problema 2 resuelto

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(frase, num_repeticiones) {
+    if (num_repeticiones < 0) {
+        return "ERROR";
+    }
+    rpta = ``
+    for (let i = 0; i < num_repeticiones; i++) {
+        rpta += frase;     
+    }
+    return rpta;
 };
 
 // Do not edit below this line
