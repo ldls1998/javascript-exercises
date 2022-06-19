02702aad0495bfa745acb84e91155f909719744e
Leonardo Loaiza <leonardodario.loaiza@unmsm.edu.pe>
2022-06-19 15:45:32 -0500

    Solución problema 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function(año) {
+    if (año % 100 == 0) {
+        año /= 100
+        return (año % 4 == 0) ? true: false;
+    }
+    return (año % 4 == 0) ? true: false;
 };
 
 // Do not edit below this line
