6c9c7406a62278b3e8f2887280c94515bd149169
Leonardo Loaiza <leonardodario.loaiza@unmsm.edu.pe>
2022-06-19 15:53:17 -0500

    Problema 7 solución

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
 const ftoc = function() {
-
+  arguments[0] = (arguments[0] - 32) * 5 / 9;
+  return Math.round(arguments[0] * 10) / 10;
 };
 
 const ctof = function() {
-
+  arguments[0] = arguments[0] * 9 / 5 + 32;
+  return Math.round(arguments[0] * 10) / 10;
 };
 
 // Do not edit below this line
