3f8217c43298de95f91386edc8b2b9e121ee5438
Leonardo Loaiza <leonardodario.loaiza@unmsm.edu.pe>
2022-06-19 14:44:51 -0500

    Solución problema 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(cadena) {
+    let myArray = cadena.split("");
+    for (let index = 0; index < myArray.length / 2; index++) {
+        const aux = myArray[index];
+        myArray[index] = myArray[myArray.length - 1 - index];
+        myArray[myArray.length - 1 - index] = aux;
+    }
+    return myArray.join("");
 };
 
 // Do not edit below this line
