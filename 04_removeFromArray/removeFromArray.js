5623143a5f7435dc9439ca31bcec581fde4a0562
Leonardo Loaiza <leonardodario.loaiza@unmsm.edu.pe>
2022-06-19 15:30:42 -0500

    Solución problema 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
 const removeFromArray = function() {
-
+    for (let i = 1; i < arguments.length; i++) {
+        const element = arguments[i];
+        const index = arguments[0].indexOf(element);
+        if (index > -1) {
+            arguments[0].splice(index, 1); // 2nd parameter means remove one item only
+          }
+    }
+    return arguments[0]
 };
 
 // Do not edit below this line
